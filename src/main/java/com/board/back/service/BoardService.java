c12c25a3096f285c7c153de21ce59b3c0512f97f
길현준 <kboxstar@gmail.com>
2021-07-28 18:53:16 +0900

    시간 입력

--- src/main/java/com/board/back/service/BoardService.java
+++ src/main/java/com/board/back/service/BoardService.java
@@ -1,5 +1,6 @@
 package com.board.back.service;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -14,12 +15,15 @@ import lombok.RequiredArgsConstructor;
 public class BoardService {
 	
 	private final BoardRepository boardRepository;
-	
+	private final SimpleDateFormat format = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
 	public List<BoardVo> getAllBoard(){
 		return boardRepository.findAll();
 	}
 
 	public BoardVo createBoard(BoardVo board) {
+		String nowTime = format.format (System.currentTimeMillis());
+		board.setCreatedTime(nowTime);
+		board.setUpdatedTime(nowTime);
 		return boardRepository.save(board);
 	}
 }
